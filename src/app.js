45bd6c945cd5a24b39f8a5efb61ae838fcfe2c44
Jean Ross <jeancmartins@icloud.com>
2020-08-19 21:34:52 -0300

    Repositories CRUD and Likes count

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,88 @@ app.use(cors());
 
 const repositories = [];
 
+function findRepositoryIndexById(id) {
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  return repositoryIndex;
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repositoryIndex = findRepositoryIndexById(id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Invalid repository ID' });
+  }
+
+  const currentRepository = repositories[repositoryIndex];
+
+  const updatedRepository = {
+    ...currentRepository,
+    title,
+    url,
+    techs,
+  };
+
+  repositories[repositoryIndex] = updatedRepository;
+
+  return response.json(updatedRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = findRepositoryIndexById(id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = findRepositoryIndexById(id);
+
+  if (repositoryIndex < 0 ) {
+    return response.status(400).json({ error: 'Invalid repository ID' });
+  }
+
+  const currentRepository = repositories[repositoryIndex];
+
+  const updatedRepository = {
+    ...currentRepository,
+    likes: currentRepository.likes + 1
+  };
+
+  repositories[repositoryIndex] = updatedRepository;
+
+  return response.json(updatedRepository);
 });
 
 module.exports = app;
